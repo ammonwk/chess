7d6edc08d84cdb2698352ea164bd4d8893a8091c
Ammon Kunzler <ammonwk@gmail.com>
2024-05-23 22:54:32 -0600

    Began implementing dataaccess

---
+++ server/src/main/java/dataaccess/InMemoryDataAccess.java
@@ -0,0 +1,65 @@
+package dataaccess;
+
+import model.*;
+import java.util.*;
+
+public class InMemoryDataAccess implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+
+    @Override
+    public void clear() {
+        users.clear();
+        auths.clear();
+        games.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        auths.remove(authToken);
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    @Override
+    public List<GameData> listGames() throws DataAccessException {
+        return new ArrayList<>(games.values());
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        games.put(game.gameID(), game);
+    }
+}
