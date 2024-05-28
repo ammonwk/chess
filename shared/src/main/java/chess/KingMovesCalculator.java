92b5c84212d8c099b2f60c2397c7241c435f88a0
Ammon Kunzler <ammonwk@gmail.com>
2024-05-27 23:19:26 -0600

    Refactored old chess code to remove duplicate logic between moveCalculators

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,26 +1,13 @@
 package chess;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class KingMovesCalculator implements PieceMovesCalculator {
+    private static final int[][] MOVES = {
+            {0, 1}, {1, 0}, {1, 1}, {-1, 0}, {-1, 1}, {-1, -1}, {0, -1}, {1, -1}
+    };
+
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
-        ArrayList<ChessMove> movesToReturn = new ArrayList<>();
-        ChessGame.TeamColor myColor = board.getPiece(myPosition).getTeamColor();
-        int[][] directions = {{0,1},{1,0},{1,1},{-1,0},{-1,1},{-1,-1},{0,-1},{1,-1}};
-        for (int[] direction : directions) {
-            if(myPosition.getRow() + direction[0] > 7
-                    || myPosition.getRow() + direction[0] < 0
-                    || myPosition.getColumn() + direction[1] > 7
-                    || myPosition.getColumn() + direction[1] < 0) {
-                continue;
-            }
-            ChessPosition peekPoint = new ChessPosition(myPosition.getRow() + direction[0] + 1,
-                                                    myPosition.getColumn() + direction[1] + 1);
-            if(board.getPiece(peekPoint) == null || board.getPiece(peekPoint).getTeamColor() != myColor) {
-                movesToReturn.add(new ChessMove(myPosition, peekPoint, null));
-            }
-        }
-        return movesToReturn;
+        return MovesCalculatorUtils.calculateMovesToSquares(board, myPosition, MOVES);
     }
 }
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,33 +1,13 @@
 package chess;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class RookMovesCalculator implements PieceMovesCalculator {
+    private static final int[][] DIRECTIONS = {
+            {1, 0}, {0, -1}, {-1, 0}, {0, 1}
+    };
+
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
-        ArrayList<ChessMove> movesToReturn = new ArrayList<>();
-        ChessGame.TeamColor myColor = board.getPiece(myPosition).getTeamColor();
-        int[][] directions = {{1,0}, {0,-1}, {-1,0}, {0,1}};
-        for (int[] direction : directions) {
-            int[] peekPoint = {myPosition.getRow(), myPosition.getColumn()};
-            while (true) {
-                peekPoint[0] += direction[0];
-                peekPoint[1] += direction[1];
-                if (peekPoint[0] > 7 || peekPoint[1] > 7 || peekPoint[0] < 0 || peekPoint[1] < 0) {
-                    break; // Don't go off the board
-                }
-                ChessPosition here = new ChessPosition(peekPoint[0] + 1, peekPoint[1] + 1); // Add 1 to convert back to 1-based index
-                if (board.getPiece(here) == null) {
-                    movesToReturn.add(new ChessMove(myPosition, here, null));
-                    // Continue
-                } else if (board.getPiece(here).getTeamColor() != myColor) {
-                    movesToReturn.add(new ChessMove(myPosition, here, null));
-                    break; // Don't move past enemy pieces after capturing
-                } else { // A team member piece
-                    break;
-                }
-            }
-        }
-        return movesToReturn;
+        return MovesCalculatorUtils.calculateMovesInDirections(board, myPosition, DIRECTIONS, false);
     }
 }
