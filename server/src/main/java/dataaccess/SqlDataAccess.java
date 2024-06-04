2d5a393f417804736925d8d337eb9c7df7749116
Ammon Kunzler <ammonwk@gmail.com>
2024-06-03 22:04:52 -0600

    added more tables

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -27,11 +27,36 @@ public class SqlDataAccess implements DataAccess{
 
     private final String[] createStatements = {
             """
+            drop table if exists users
+            drop table if exists auths
+            drop table if exists games
+            
+            
             CREATE TABLE IF NOT EXISTS users (
               `id` int NOT NULL AUTO_INCREMENT,
-              `name` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL,
               `email` varchar(256) NOT NULL,
               `password` varchar(256) NOT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(email),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+            CREATE TABLE IF NOT EXISTS auths (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `username` varchar(256) NOT NULL,
+              `authtoken` varchar(256) NOT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(email),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+            CREATE TABLE IF NOT EXISTS games (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `gameName` varchar(256) NOT NULL,
+              `gameId` varchar(256) NOT NULL,
+              `playerColor` ENUM('white', 'black') DEFAULT 'white',
+              `gamestate` varchar(256) NOT NULL,
               PRIMARY KEY (`id`),
               INDEX(email),
               INDEX(name)
