71b0ff44dd9c9041972ad77ad003d21dcf9e8f4a
Ammon Kunzler <ammonwk@gmail.com>
2024-06-10 09:01:54 -0600

    Skeleton of client menu

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import chess.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.printf("♕ Welcome to Chess! Please enter a number to choose an option:\n"
+                + "(1) Help\n" + "(2) Login\n" + "(3) Register\n" + "(4) Quit\n" + "> ");
+
+        try {
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 4:
+                    exit = true;
+                    System.out.println("Thanks for playing! Goodbye");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a number between 1 and 4");
+
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a number.");
+            scanner.next(); // clear the invalid input
+        }
+        }
     }
 }
