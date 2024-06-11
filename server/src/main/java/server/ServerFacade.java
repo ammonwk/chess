2b7a10d9dfc60cc32440a8a91d9b5b1ac93e736a
Ammon Kunzler <ammonwk@gmail.com>
2024-06-10 21:53:40 -0600

    Filled out skeleton of ServerFacade

--- server/src/main/java/server/ServerFacade.java
+++ server/src/main/java/server/ServerFacade.java
@@ -1,4 +1,8 @@
 package server;
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import service.*;
+import spark.Spark;
 
 import java.io.*;
 import java.net.*;
@@ -10,5 +14,99 @@ public class ServerFacade {
     public ServerFacade(String url) {
         serverUrl = url;
     }
-    
+
+//    Spark.delete("/db", new ClearHandler(dataAccess));
+//    Spark.post("/user", new RegisterHandler(new UserService(dataAccess)));
+//    Spark.post("/session", new LoginHandler(new UserService(dataAccess)));
+//    Spark.delete("/session", new LogoutHandler(new UserService(dataAccess)));
+//    Spark.get("/game", new ListGamesHandler(new GameService(dataAccess)));
+//    Spark.post("/game", new CreateGameHandler(new GameService(dataAccess)));
+//    Spark.put("/game", new JoinGameHandler(new GameService(dataAccess)));
+
+    public void clearDatabase() throws DataAccessException {
+        var path = "/db";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    public RegisterResult registerUser(RegisterRequest req) throws DataAccessException {
+        var path = "/user";
+        return this.makeRequest("POST", path, req, RegisterResult.class);
+    }
+
+    public LoginResult loginUser(LoginRequest req) throws DataAccessException {
+        var path = "/session";
+        return this.makeRequest("POST", path, req, LoginResult.class);
+    }
+
+    public ClearResult logoutUser(String auth) throws DataAccessException {
+        var path = "/session";
+        return this.makeRequest("DELETE", path, auth, null);
+    }
+
+    public ListGamesResult listGames(String auth) throws DataAccessException {
+        var path = "/user";
+        return this.makeRequest("GET", path, auth, ListGamesResult.class);
+    }
+
+    public CreateGameResult createGame(CreateGameRequest req) throws DataAccessException {
+        var path = "/user";
+        return this.makeRequest("POST", path, req, CreateGameResult.class);
+    }
+
+    public JoinGameResult joinGame(JoinGameRequest req) throws DataAccessException {
+        var path = "/game";
+        return this.makeRequest("PUT", path, req, JoinGameResult.class);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws DataAccessException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, DataAccessException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new DataAccessException("failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
 }
